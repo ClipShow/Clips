82edce051cc0943fba11f0d4210416aeeb5ff164
ClipShow <cory.proudfit@gmail.com>
2025-05-30 22:16:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    print("✅ Hello route was hit")
-    return "Hello from minimal app", 200
+def home():
+    return "Server is running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
